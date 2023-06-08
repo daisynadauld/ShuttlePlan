4d83a9e619fa92669c6dad730f9b52c23b78021d
Daisy Nadauld <92403835+daisynadauld@users.noreply.github.com>
2023-06-07 18:08:41 -0600

    outline

--- event_creator.py
+++ event_creator.py
@@ -1,1 +1,18 @@
+# event_creator.py
+from googleapiclient.discovery import build
 
+class EventCreator:
+    def __init__(self, credentials):
+        self.service = build('calendar', 'v3', credentials=credentials)
+
+    def create_event(self, event_data):
+        # Implement the logic to create a Google Calendar event
+        # Use the self.service object to interact with the API
+        # Pass the event_data as a parameter to create the event
+
+        event = self.service.events().insert(calendarId='primary', body=event_data).execute()
+        event_id = event.get('id')
+
+        return event_id
+
+    # Implement other methods related to event interactions, if required
